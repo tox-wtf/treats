ffec26e49aa91a1cfa012b6568c5a87dfbb8166a
tox <tox@tox.wtf>
2026-04-27 15:05:18 -0500

    chore: tweaks and fmt

--- src/lib.rs
+++ src/lib.rs
@@ -2,25 +2,19 @@
 
 #![cfg_attr(not(feature = "std"), no_std)]
 
-#[cfg(feature = "path_to_string")]
-mod path;
-#[cfg(feature = "path_to_string")]
-pub use path::*;
+#[cfg(feature = "std")] extern crate std;
 
-#[cfg(feature = "inspect_none")]
-mod inspect_none;
-#[cfg(feature = "inspect_none")]
-pub use inspect_none::*;
+#[cfg(feature = "path_to_string")] mod path;
+#[cfg(feature = "path_to_string")] pub use path::*;
 
-#[cfg(feature = "discard")]
-mod discard;
-#[cfg(feature = "discard")]
-pub use discard::*;
+#[cfg(feature = "inspect_none")] mod inspect_none;
+#[cfg(feature = "inspect_none")] pub use inspect_none::*;
 
-#[cfg(feature = "permit")]
-mod permit;
-#[cfg(feature = "permit")]
-pub use permit::*;
+#[cfg(feature = "discard")] mod discard;
+#[cfg(feature = "discard")] pub use discard::*;
+
+#[cfg(feature = "permit")] mod permit;
+#[cfg(feature = "permit")] pub use permit::*;
 
 #[cfg(test)]
 #[macro_use]
